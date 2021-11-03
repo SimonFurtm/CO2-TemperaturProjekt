87c147c59e609f2fa746084c174f32734e2c20f4
SimonFurtm <82949946+SimonFurtm@users.noreply.github.com>
2021-11-03 10:29:16 +0100

    web

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import NavBar from './components/navbar';
+import { Button } from 'react-bootstrap';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar /> 
         <header className="App-header">
+          
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">COZwei</h1>
         </header>
+        
         <Customers />
       </div>
     );
