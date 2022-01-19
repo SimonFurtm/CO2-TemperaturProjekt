a7156e2d4b03321ddff60e95cd89e470b5d96bd1
SimonFurtm <82949946+SimonFurtm@users.noreply.github.com>
2022-01-19 10:30:51 +0100

    19.01

--- website/frontend/src/index.js
+++ website/frontend/src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//const sgMail = require('@sendgrid/mail');
+
+//sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+/* msg = {
+  to: 'simon.furtmueller@htl-saalfelden.at',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};*/
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  
+  //sgMail.send(msg)
 );
 
 // If you want to start measuring performance in your app, pass a function
