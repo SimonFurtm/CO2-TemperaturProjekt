75798c9033b41e1d541a7817869474b4ea797b7e
Manuel-Schwaiger <mansenswag@gmail.com>
2022-01-19 10:23:18 +0100

    19.01

--- website/frontend/src/pages/grenzwerte.js
+++ website/frontend/src/pages/grenzwerte.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Chart from "../components/chart";
+import Button from 'react-bootstrap/Button';
 
 
 const Grenzwerte = () => {
@@ -10,9 +11,32 @@ const Grenzwerte = () => {
     return (
         <div>
             <h1>Grenzwerte</h1>
-            <p>This Page is the Grenzwerte Page. Du konnst des mani</p>
-            
-            
+            <p>Legen Sie ihre Grenzwerte fest</p>
+
+<form>
+
+<div class="formgroup" id="email-form">
+    <label for="email">Your e-mail*</label>
+    <input type="email" id="email" name="email" />
+</div>
+
+<div class="formgroup" id="name-form">
+    <label for="name">CO2</label>
+    <input type="text" id="name" name="name" />
+</div>
+
+<div class="formgroup" id="name-form">
+    <label for="name">Luftfeuchtigkeit</label>
+    <input type="text" id="name" name="name" />
+</div>
+
+
+<div class="formgroup" id="name-form">
+    <label for="name">Temperatur</label>
+    <input type="text" id="name" name="name" />
+</div>
+        <input type="submit" value="Submit" />
+    </form>  
         </div>
     )
 }
