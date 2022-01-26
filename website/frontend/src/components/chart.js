85ee3e5d15cb7b83c080efcf5b5152858770a5f0
Chris2300AT <82949838+Chris2300AT@users.noreply.github.com>
2022-01-26 10:49:20 +0100

    mago der boss

--- website/frontend/src/components/chart.js
+++ website/frontend/src/components/chart.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import formate from'../components/data/formate'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-var data = [];
+var data3 = [];
 
 
 class Chart extends Component {
@@ -21,17 +22,43 @@ class Chart extends Component {
     }
 
   render(){
-    {this.state.chartData.map(chartData =>
-      data = [
-        { name: "12.01.22", uv: 400},
-        { name: "13.01.22", uv: 413},
-        { name: "14.01.22", uv: 300},
-        { name: "15.01.22", uv: 1000},
-      ]
-      )}
+    console.log("Render")
+    var tmp = this.state.chartData.map((chartData) => ({
+       name: formate.changeDatumFormat(chartData.Datum), uv: chartData.CO2
+    }));
+
+    console.log(tmp)
+    data3 = tmp;
+
+    console.log(data3)
+    
+       var data2 = [
+         { name: "12.01.2022", uv: 100},
+         { name: "13.01.2022", uv: 1000},
+         { name: "14.01.2022", uv: 300},
+         { name: "15.01.2022", uv: 1000},
+         { name: "12.01.2022", uv: 100},
+         { name: "13.01.2022", uv: 1000},
+         { name: "14.01.2022", uv: 300},
+         { name: "15.01.2022", uv: 1000},
+         { name: "12.01.2022", uv: 100},
+         { name: "13.01.2022", uv: 1000},
+         { name: "14.01.2022", uv: 300},
+         { name: "15.01.2022", uv: 1000},
+         { name: "12.01.2022", uv: 100},
+         { name: "13.01.2022", uv: 1000},
+         { name: "14.01.2022", uv: 300},
+         { name: "15.01.2022", uv: 1000},
+         { name: "13.01.2022", uv: 1000},
+         { name: "14.01.2022", uv: 300},
+         { name: "15.01.2022", uv: 1000},
+       ]
+       console.log(data2)
+      
+      
     return (
 <div>
-    <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <LineChart width={600} height={300} data={data3} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="name" />
